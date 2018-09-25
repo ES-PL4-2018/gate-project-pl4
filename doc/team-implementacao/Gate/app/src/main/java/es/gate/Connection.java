aea531707d7b4bede0debaae1663f59271abe29a
Alexandre Brito <alex_brito_97@hotmail.com>
2018-09-25 17:30:29 +0100

    Connection to Server
    Client connects to server, thread dies afterwards
    Baseline connection

--- doc/team-implementacao/Gate/app/src/main/java/es/gate/Connection.java
+++ doc/team-implementacao/Gate/app/src/main/java/es/gate/Connection.java
@@ -4,30 +4,30 @@ import android.os.AsyncTask;
 import java.io.IOException;
 import java.net.Socket;
 
-public class Connection extends AsyncTask<Void, Void, Void> {
+public class Connection implements Runnable{
 
-    String serverAddress;
-    int serverPort;
+    private String serverAddress;
+    private int serverPort;
+    private Socket cltSocket = null;
 
     public Connection(String addr, int port) {
 
         serverAddress = addr;
         serverPort = port;
+        new Thread(this).start();
     }
 
-    @Override
-    protected Void doInBackground(Void... arg0) {
 
-        System.out.println("lalallaallaallalallalalala");
-        Socket cltSocket = null;
+    public void run(){
 
         try {
             cltSocket = new Socket(serverAddress, serverPort);
             System.out.println("Connection attempted");
+
+
         }catch(IOException e){
             System.out.println("IO" + e);
         }
 
-        return null;
     }
 }
