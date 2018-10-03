96ebbe8e1b31ee99849c8b767303c9b708dc1c4c
Alexandre Brito <43242903+AlexdBrito@users.noreply.github.com>
2018-10-03 20:57:30 +0100

    Update Server.java

--- src/GateServer/Server.java
+++ src/GateServer/Server.java
@@ -1,30 +1,153 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.net.Socket;
-import java.*;
 import java.util.ArrayList;
 
-public class Server {
+public class Client implements Runnable {
 
-    public static void main(String args[]){
+    DataInputStream in;
+    DataOutputStream out;
+    Socket clientSocket;
+    ArrayList<Account> accounts;
+    Account curUser = null;
 
-        ArrayList<Account> accountsList = new ArrayList<>();
+    public Client(Socket cltSocket, ArrayList<Account> acc) {
+        clientSocket = cltSocket;
+        accounts = acc;
+        System.out.println("New User");
+        new Thread(this).start();
+    }
+
+    public void run() {
 
         try {
-            DataInputStream in;
-            DataOutputStream out;
-            int serverPort = 42069;
-            ServerSocket serverSocket = new ServerSocket(serverPort);
+            DataInputStream readClient = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream writeClient = new DataOutputStream(clientSocket.getOutputStream());
+            //Auxiliary variables to facilitate code
+            int auxInt;
+            String auxStr;
+            String[] strAr;
 
-            while(true){
-                new Client(serverSocket.accept(), accountsList);
-                System.out.println("New Client");
+
+            while (true) {
+
+                auxInt = 0;
+
+                auxStr = readClient.readUTF();  //Reads a message from the client
+                System.out.println(auxStr);
+                strAr = auxStr.split("&");    //Client messages come formatted with info separated by "&"
+                switch (strAr[0]) {
+                    case "!login": {
+                        System.out.println("hi");
+                        for (Account acc : accounts) {
+                            if (acc.checkLoginInfo(strAr[1], strAr[2])) {
+                                writeClient.writeUTF("1");
+                                curUser = acc;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    case "!checkUser": {
+                        for (Account acc : accounts) {
+                            auxInt = acc.checkDuplicateName(strAr[1]);
+                            if (auxInt == 1) {
+                                break;
+                            }
+                        }
+                        auxStr = String.format("%d", auxInt);
+                        writeClient.writeUTF(auxStr);
+                        System.out.println(auxStr);
+                        break;
+                    }
+                    case "!checkEmail": {
+                        for (Account acc : accounts) {
+                            auxInt = acc.checkDuplicateEmail(strAr[1]);
+                            if (auxInt == 1) {
+                                break;
+                            }
+                        }
+                        auxStr = String.format("%d", auxInt);
+                        writeClient.writeUTF(auxStr);
+                        System.out.println(auxStr);
+                        break;
+                    }
+
+                    case "!checkOrcid": {
+                        for (Account acc : accounts) {
+                            auxInt = acc.checkDuplicateORCID(strAr[1]);
+                            if (auxInt == 1) {
+                                break;
+                            }
+                        }
+                        auxStr = String.format("%d", auxInt);
+                        writeClient.writeUTF(auxStr);
+                        System.out.println(auxStr);
+                        break;
+                    }
+
+                    case "!register":{
+
+                        curUser = new Account(strAr[1], strAr[2], strAr[3], Long.parseLong(strAr[4]), strAr[5], strAr[6], strAr[7], strAr[8], strAr[9], strAr[10]);
+
+                        accounts.add(curUser);
+                        System.out.println("New Registry: " + strAr[1]);
+                        writeClient.writeUTF(String.format("1"));
+                        break;
+                    }
+
+                    case "!getFeedInfo":{
+
+                        if(curUser == null){
+                            auxStr = String.format("%s", "Test User");
+                        }else{
+                            auxStr = curUser.getName();
+                        }
+                        writeClient.writeUTF(auxStr);
+                        break;
+                    }
+
+                    case "!getProfileInfo": {
+                        if(curUser == null){
+                            auxStr = String.format("%s&%s&%s&%s&%s", "Test User", "Test Inst", "Test Unit", "Test Date", "Test Interests");
+                        }else{
+                            auxStr = curUser.getProfileInfo();
+                        }
+                        writeClient.writeUTF(auxStr);
+                        break;
+                    }
+                    case "!setInfo":{
+
+                        if(curUser == null)
+                            break;
+                        switch (strAr[1]){
+                            case "Inst":{
+                                curUser.setInstitution(strAr[2]);
+                                break;
+                            }
+
+                            case "Unit":{
+                                curUser.setResearchUnits(strAr[2]);
+                                break;
+                            }
+
+                            case "Date":{
+                                curUser.setBirthDate(strAr[2]);
+                                break;
+                            }
+
+                            case "Inter": {
+                                curUser.setThemesInterest(strAr[2]);
+                                break;
+                            }
+                        }
+                    }
+                }
             }
-        }catch(IOException e){
-            //TODO
-        }
 
+        } catch (IOException e) {
+            System.err.println("IOException on read Thread" + e);
+            //curClient.setLog(false);
+        }
     }
 }
