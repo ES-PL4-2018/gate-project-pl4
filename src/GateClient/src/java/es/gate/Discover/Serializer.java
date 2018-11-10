c3577cc876cf9ac97c24f4a5940ebb9337bc36ef
jpgoncalves97 <jpmg411@gmail.com>
2018-11-10 16:15:39 +0000

    Multicast/Serializer/Discover contacts

---
+++ src/GateClient/src/java/es/gate/Discover/Serializer.java
@@ -0,0 +1,38 @@
+package es.gate.Discover;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Serializer {
+
+    /**
+     * Simple serializer that converts given object as parameter to a byte array
+     *
+     * @param obj
+     * @return a byte array
+     * @throws IOException
+     */
+    public static byte[] serialize(Object obj) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(out);
+        os.writeObject(obj);
+        return out.toByteArray();
+    }
+
+    /**
+     * Simple deserializer that converts given byte array as parameter to an object
+     *
+     * @param data
+     * @return converted object
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream in = new ByteArrayInputStream(data);
+        ObjectInputStream is = new ObjectInputStream(in);
+        return is.readObject();
+    }
+}
