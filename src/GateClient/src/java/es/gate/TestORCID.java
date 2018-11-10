b53b8fe02549f91220f5365afc6690c51028bf54
Flávio Fernandes <33881646+flavioafernandes@users.noreply.github.com>
2018-11-10 20:22:50 +0000

    Info Orcid para Preencher Registo

--- src/GateClient/src/java/es/gate/TestORCID.java
+++ src/GateClient/src/java/es/gate/TestORCID.java
@@ -1,8 +1,13 @@
 package es.gate;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 import com.google.gson.Gson;
 import com.mopub.common.util.Json;
@@ -57,11 +62,44 @@ public class TestORCID {
         return response.body().string();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static HashMap createHash(String input) throws IOException {
+        String propertiesFormat = input.replaceAll(",", "\n");
+        Properties properties = new Properties();
+        properties.load(new StringReader(propertiesFormat));
+        Set<Map.Entry<Object, Object>> entrySet = properties.entrySet();
+        HashMap<String,String > map = new HashMap<String, String>();
+        for (Iterator<Map.Entry<Object, Object>> it = entrySet.iterator(); it.hasNext();) {
+            Map.Entry<Object,Object> entry = it.next();
+            map.put((String)entry.getKey(), (String)entry.getValue());
+        }
+        return map;
+    }
 
+    public static String getKeywords(TestORCID example, String orcid) throws IOException {
+        String getResponse = example.doGetRequest("https://pub.orcid.org/v2.1/" + orcid + "/keywords");
+        String keywords = "";
+        Gson gson = new Gson();
+        HashMap a = gson.fromJson(getResponse, HashMap.class);
+        String hash = a.toString();
+        String[] str = hash.split(",");
+        for (int i=0;i<str.length;i++){
+            if(str[i].contains("content=")){
+                keywords += str[i].replace("content=","") + ",";
+            }
+        }
+        if(keywords.length()>=3){
+            keywords = keywords.substring(0,keywords.length()-1);
+        }
+
+        return keywords;
+    }
+
+    public static void returnOrcidInfo(String orcid) throws IOException {
         // issue the Get request
         TestORCID example = new TestORCID();
-        String getResponse = example.doGetRequest("https://pub.orcid.org/v2.0/0000-0002-1825-0097/activities");
+        String getResponse = example.doGetRequest("https://pub.orcid.org/v2.1/" + orcid);
+        String keywords = getKeywords(example,orcid);
+        String name ="",lastName="",organization="";
         //System.out.println(getResponse);
 
 
@@ -72,6 +110,36 @@ public class TestORCID {
 
         Gson gson = new Gson();
         HashMap a = gson.fromJson(getResponse, HashMap.class);
-        System.out.println(a);
+        HashMap h = createHash(a.toString());
+
+        try{
+            name = h.get("given-names").toString();
+            lastName = h.get("family-name").toString();
+        }catch (NullPointerException n){
+
+        }
+        try{
+            organization = h.get("organization").toString();
+        }catch (NullPointerException n){
+
+        }
+
+
+        name = name.replace("{value=", "").replace("}", "");
+        lastName = lastName.replace("{value=", "").replace("}", "");
+        organization = organization.replace("{name=","");
+
+        System.out.println("First Name: " + name);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Organization: " + organization);
+        System.out.println("Keywords: " + keywords);
+        System.out.println("Email: " + h.get("email"));
+        //se o email nao existe pode dar return de [] ou null
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        returnOrcidInfo("0000-0002-6122-3035");
     }
 }
